7d3b0782c9c710aa64b66af5a3630f2525ecd7a3
nataly <natalygiron18@outlook.com>
2024-10-28 16:08:39 -0500

    First commit

---
+++ src/main/java/com/project/coffeexpressapp/CoffeexpressappApplication.java
@@ -0,0 +1,13 @@
+package com.project.coffeexpressapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeexpressappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeexpressappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/coffeexpressapp/CoffeexpressappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.coffeexpressapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeexpressappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
